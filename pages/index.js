c40d04f585eeb3824f3f4d1e372aca7278a87d4b
hieuhmle <hieuhmle@gmail.com>
2021-08-29 18:32:52 -0500

    Fetching data on the server with getInitialProps

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,25 @@
-function Home() {
+import { useEffect } from 'react'
+import axios from 'axios'
+
+function Home({ products}) {
+  console.log(products)
+  // useEffect(() => {
+  //   getProducts()
+  // }, [])
+
+  // async function getProducts() {
+  //   const url = 'http://localhost:3000/api/products'
+  //   const response = await axios.get(url)
+  //   console.log(response)
+  // }
+
   return <>home</>;
+}
+
+Home.getInitialProps = async () => {
+  const url = 'http://localhost:3000/api/products'
+  const response = await axios.get(url)
+  return { products: response.data}
 }
 
 export default Home;
