6ddfe85347e079c70c28f7d765e9a68a9e192528
hieuhmle <hieuhmle@gmail.com>
2021-08-30 19:52:31 -0500

    Create Products Collection, model Product Data

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,8 @@
 import { useEffect } from 'react'
 import axios from 'axios'
 
-function Home({ products}) {
-  // console.log(products)
-  // useEffect(() => {
-  //   getProducts()
-  // }, [])
-
-  // async function getProducts() {
-  //   const url = 'http://localhost:3000/api/products'
-  //   const response = await axios.get(url)
-  //   console.log(response)
-  // }
-
+function Home({ products }) {
+  console.log(products)
   return <>home</>;
 }
 
