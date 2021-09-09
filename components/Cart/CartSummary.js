828248c359481991b9c7cd82150987e8a713a9af
hieuhmle <hieuhmle@gmail.com>
2021-09-08 20:35:18 -0500

    Style cart products, calculate total

--- components/Cart/CartSummary.js
+++ components/Cart/CartSummary.js
@@ -1,11 +1,28 @@
 import { Button, Segment, Divider } from 'semantic-ui-react'
+import { useEffect, useState } from 'react';
+import calculateCartTotal from '../../utils/calculateCartTotal'
 
-function CartSummary() {
+function CartSummary({ products }) {
+  const [isCartEmpty, setCartEmpty] = useState(false)
+  const [cartAmount, setCartAmount] = useState(0)
+  const [stripeAmount, setStipeAmount] = useState(0)
+
+  useEffect(() => {
+    const { cartTotal, stripeTotal } = calculateCartTotal(products)
+    setCartAmount(cartTotal)
+    setStipeAmount(stripeTotal)
+    if (products.length === 0) {
+      setCartEmpty(true)
+    }
+  }, [products])
+
   return <>
     <Divider/>
     <Segment clearing size='large'>
-      <strong>Sub total:</strong> $0.00
-      <Button icon='cart' color='teal' floated='right' content='Checkout'/>
+      <strong>Sub total:</strong> ${cartAmount}
+      <Button icon='cart' 
+      disabled={isCartEmpty}  
+      color='teal' floated='right' content='Checkout'/>
     </Segment>
   </>
 }
